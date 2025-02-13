928639287244b914cff55807882ee17872c437ac
Landon Shumway <landon@inspiringapps.com>
2025-02-13 10:18:18 -0600

    Including transaction status in csv reports

--- backend/compact-connect/lambdas/python/purchases/handlers/transaction_reporting.py
+++ backend/compact-connect/lambdas/python/purchases/handlers/transaction_reporting.py
@@ -437,18 +437,19 @@ def _generate_compact_transaction_report(transactions: list[dict], providers: di
             'Licensee First Name',
             'Licensee Last Name',
             'Licensee Id',
-            'Transaction Settlement Date',
+            'Transaction Settlement Date UTC',
             'State',
             'State Fee',
             'Administrative Fee',
             'Collected Transaction Fee',
             'Transaction Id',
             'Privilege Id',
+            'Transaction Status'
         ]
     )
 
     if not transactions:
-        writer.writerow(['No transactions for this period'] + [''] * 9)
+        writer.writerow(['No transactions for this period'] + [''] * 10)
         return output.getvalue()
 
     for transaction in transactions:
@@ -479,6 +480,7 @@ def _generate_compact_transaction_report(transactions: list[dict], providers: di
                         transaction_fee_item['unitPrice'] if transaction_fee_item else '0',
                         transaction['transactionId'],
                         item.get('privilegeId', 'UNKNOWN'),
+                        transaction['transactionStatus'],
                     ]
                 )
 
@@ -512,20 +514,21 @@ def _generate_jurisdiction_reports(
                 'First Name',
                 'Last Name',
                 'Licensee Id',
-                'Transaction Settlement Date',
+                'Transaction Settlement Date UTC',
                 'State Fee',
                 'State',
                 'Compact Fee',
                 'Transaction Id',
                 'Privilege Id',
+                'Transaction Status'
             ]
         )
 
         if not trans_items:
-            writer.writerow(['No transactions for this period'] + [''] * 8)
-            writer.writerow([''] * 9)
-            writer.writerow(['Privileges Purchased', 'Total State Amount'] + [''] * 7)
-            writer.writerow(['0', '$0.00'] + [''] * 7)
+            writer.writerow(['No transactions for this period'] + [''] * 9)
+            writer.writerow([''] * 10)
+            writer.writerow(['Privileges Purchased', 'Total State Amount'] + [''] * 8)
+            writer.writerow(['0', '$0.00'] + [''] * 8)
             reports[jurisdiction] = output.getvalue()
             continue
 
@@ -549,6 +552,7 @@ def _generate_jurisdiction_reports(
                     compact_fee_item['unitPrice'],
                     transaction['transactionId'],
                     item.get('privilegeId', 'UNKNOWN'),
+                    transaction['transactionStatus'],
                 ]
             )
 
@@ -558,9 +562,9 @@ def _generate_jurisdiction_reports(
             total_amount += float(item['unitPrice']) * int(float(item['quantity']))
 
         # Add summary rows
-        writer.writerow([''] * 9)
-        writer.writerow(['Privileges Purchased', 'Total State Amount'] + [''] * 7)
-        writer.writerow([int(total_privileges), f'${total_amount:.2f}'] + [''] * 7)
+        writer.writerow([''] * 10)
+        writer.writerow(['Privileges Purchased', 'Total State Amount'] + [''] * 8)
+        writer.writerow([int(total_privileges), f'${total_amount:.2f}'] + [''] * 8)
 
         reports[jurisdiction] = output.getvalue()
 
