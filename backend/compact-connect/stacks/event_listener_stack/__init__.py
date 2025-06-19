bd75c545e28a997079c75c031caba4f685dbfe5e
Landon Shumway <landon@inspiringapps.com>
2025-06-19 10:34:12 -0700

    Fix smoke tests to use API to verify provider details

--- backend/compact-connect/stacks/event_listener_stack/__init__.py
+++ backend/compact-connect/stacks/event_listener_stack/__init__.py
@@ -45,7 +45,7 @@ class EventListenerStack(AppStack):
             lambda_dir='data-events',
             index=os.path.join('handlers', 'encumbrance_events.py'),
             handler='license_encumbrance_listener',
-            timeout=Duration.minutes(15),
+            timeout=Duration.minutes(2),
             environment={
                 'PROVIDER_TABLE_NAME': persistent_stack.provider_table.table_name,
                 'EMAIL_NOTIFICATION_SERVICE_LAMBDA_NAME': persistent_stack.email_notification_service_lambda.function_name,  # noqa: E501 line-too-long
@@ -93,7 +93,7 @@ class EventListenerStack(AppStack):
             lambda_dir='data-events',
             index=os.path.join('handlers', 'encumbrance_events.py'),
             handler='license_encumbrance_lifted_listener',
-            timeout=Duration.minutes(15),
+            timeout=Duration.minutes(2),
             environment={
                 'PROVIDER_TABLE_NAME': persistent_stack.provider_table.table_name,
                 **self.common_env_vars,
@@ -139,7 +139,7 @@ class EventListenerStack(AppStack):
             lambda_dir='data-events',
             index=os.path.join('handlers', 'license_deactivation_events.py'),
             handler='license_deactivation_listener',
-            timeout=Duration.minutes(15),
+            timeout=Duration.minutes(2),
             environment={
                 'PROVIDER_TABLE_NAME': persistent_stack.provider_table.table_name,
                 **self.common_env_vars,
